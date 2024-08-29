72d09828824a32adb64b51eb309acd5cbc9ee1d1
windwhisper <1309896340@qq.com>
2024-08-29 11:55:26 +0800

    重新梳理了右侧表达式的语法关系

--- README.md
+++ README.md
@@ -2,3 +2,99 @@
 
 D0L-System : 确定性、上下文无关
 
+<br/>
+
+
+
+# 实现一个简单的D0L-System
+
+示例：雪花分形
+
+`Varname: F`
+
+`Axiom: F`
+
+`Production: F -> F+F--F+F`
+
+需要用lexy解析这个表达式
+
+## 类设计
+
+首先，对LSystem的初始化，需要注册所有会用到的变量名，例如这里的 `F` 就是变量名
+
+为LSystem类添加一个 `string iterate(const string &p)` 的方法，这个方法用于对字符串进行迭代计算
+
+增加`string tmp` 用来存储中间量，增加 `string next()` 方法用于调用 `iterate` 更新 `tmp` 并返回，增加 `void reset()` 方法用于重置 `tmp` 以及其他所有迭代过程中可能的状态量
+
+但是这么做，显得 `iterate()` 太过独立，但它要做的事相当明确，就是由一个输入串，根据LRule进行替换，生成新串。将 `iterate()` 独立出来的好处，是可以通过将其指定为虚函数，以派生不同规则的LSystem子类
+
+## 初步构思实现
+
+D0LSystem的 `iterate()` 实现
+
+将Production拆解为两个部分，与map的命名保持一致，左半部分称为 `key`，右半部分称为 `value`
+
+`key`的语法，先举个例子，它可以是 `F(0, 1, 2, 3, 4.3)AC(3, 1.1, 2)`
+
+其中 `F` 有5个参数，参数数量这是确定的，在最开始的 `Varname` 中就已经定义好的。后面紧跟的 `A` 可能存在歧义，它可能是一个无参数的独立变量，也可能是一个带3参数的名为 `AC` 的变量，区分它们的方法也是看 `Varname` 中的定义
+
+如此分析，每个在 `Varname` 出现的变量都应该表示成一个函数，但是解析的函数应该如何在代码中实现？首先，它由一个函数名，和若干参数构成，两者是一体的，不能分开写，因此上文中的 LRule 的定义就不合适，应当用一个字符串整体来定义，就像matlab的 `function res = xxx()` 这样，我可以将它的语法规定为比如 `xxx(x, y, ...) -> yyy(y, x, ...)zzz(0.1x, 0.2y)`
+
+注意到这个语法的特点，`key` 部分只有一个带参符号，但是 `value` 部分可以由多个带参符号构成，这类文法应该有个名字，可能叫 `LL(1)` 型文法，需要搜索了解一下
+
+注意到一个问题，varname和production中对于符号的唯一性管理，对于一个带参数的符号，它应当是在production中进行具体的参数映射的定义，但是它同时也应该在varname中进行声明，以保证它和不带参数的符号不会发生冲突
+
+重新考虑了一下，将varname直接删去，LSystem类直接通过解析LRule中的 `key` 来注册所有非终结符号1。但这么做的话，需要另外设计一个用于最终渲染的解析器。
+
+
+先整理一下 `key` 部分的语法格式。首先，它只包含一个符号，这个符号可以带参数，也可以不带参数
+
+注册符号表似乎不需要传递额外的参数，只需要解析 `LRule` 的 `key` 即可
+
+
+
+## 语法规则整理
+
+### 解析L-System产生式的定义
+
+`Sym` : 产生式的左半部分
+
+`SymName` : `Sym` 的名称部分，以字母、下划线开头，后续字母、数字、下划线的组合
+
+`ParameterList` : 由圆括号包裹任意数量个 `ParamItem` 构成
+
+`ParamItem` : 符号的参数项，指定 `Sym` 的形式参数名，以字母、下划线开头，后续字母、数字、下划线的组合
+
+
+<br/>
+
+`SymMap` : 产生式的右半部分，同样以 `SymName` 开头并紧跟一个可选择的 `ParameterMappedList`，但是其item定义有所区别，映射到的目标是原 `ParamItem` 构成的表达式
+
+`ParameterMappedList` : 左侧形式参数映射到的右侧的参数列表，其中的每一项为 `MathExpr`
+
+`MathExpr` : 数学表达式，由 `ParamItem`、`Number`、 递归的 `MathExpr` 构成
+
+
+
+
+## 构建语法树
+
+表达式的作用域
+```cpp
+    struct environment{
+        // 解析出来的L-System 生成式
+
+    };
+```
+
+数学表达式语法树的基类
+
+```cpp
+    struct expr{
+        virtual ~expr()=default;
+        virtual float evaluate() const = 0;
+    };
+```
+
+
+
--- include/lexy_parser.hpp
+++ include/lexy_parser.hpp
@@ -10,29 +10,31 @@ namespace {
 using namespace std;
 
 // =================================值构造========================================
-struct Psym { // 每一个带参符号，都由符号名、参数列表(参数指针，参数个数)构成
-  string sym_name;
-  optional<vector<string>> params; // 形式参数的名字
+struct Psym {  // 每一个带参符号，都由符号名、参数列表(参数指针，参数个数)构成
+    string sym_name;
+    optional<vector<string>> params;  // 形式参数的名字
 
-  Psym() : sym_name(""), params({}) {}
-  Psym(const string &name, const optional<vector<string>> &params)
-      : sym_name(name), params(params) {}
+    Psym()
+        : sym_name(""), params({}) {}
+    Psym(const string& name, const optional<vector<string>>& params)
+        : sym_name(name), params(params) {}
 };
 
 struct Number {
-  int int_part;
-  optional<int> fraction_part;
-  Number(const int a, const optional<int> &b) : int_part(a), fraction_part(b) {}
-  float value() const {
-    float res = static_cast<float>(int_part);
-    if (fraction_part.has_value()) {
-      float tmp = static_cast<float>(fraction_part.value());
-      while (tmp > 0)
-        tmp /= 10.0f;
-      res += tmp;
+    int int_part;
+    optional<int> fraction_part;
+    Number(const int a, const optional<int>& b)
+        : int_part(a), fraction_part(b) {}
+    float value() const {
+        float res = static_cast<float>(int_part);
+        if (fraction_part.has_value()) {
+            float tmp = static_cast<float>(fraction_part.value());
+            while (tmp > 0)
+                tmp /= 10.0f;
+            res += tmp;
+        }
+        return res;
     }
-    return res;
-  }
 };
 
 // // 函数由函数名、参数列表(实参名，所有实参的表达式) 构成
@@ -40,65 +42,102 @@ struct Number {
 
 // =================================解析规则========================================
 namespace dsl = lexy::dsl;
+constexpr auto escaped_newline = dsl::backslash >> dsl::newline;
 struct SymName {
-  static constexpr auto rule = dsl::identifier(
-      dsl::ascii::alpha / dsl::lit_c<'_'>, dsl::ascii::alnum / dsl::lit_c<'_'>);
-  static constexpr auto value = lexy::as_string<string>;
+    static constexpr auto rule = dsl::identifier(
+        dsl::ascii::alpha / dsl::lit_c<'_'>,
+        dsl::ascii::alnum / dsl::lit_c<'_'>);
+    static constexpr auto value = lexy::as_string<string>;
 };
 
 struct ParameterItem {
-  static constexpr auto whitespace = dsl::ascii::space; // 设置自动跳空格
-  static constexpr auto rule = dsl::identifier(
-      dsl::ascii::alpha / dsl::lit_c<'_'>, dsl::ascii::alnum / dsl::lit_c<'_'>);
-  static constexpr auto value = lexy::as_string<string>;
+    static constexpr auto whitespace = dsl::ascii::space;  // 设置自动跳空格
+    static constexpr auto rule = dsl::identifier(
+        dsl::ascii::alpha / dsl::lit_c<'_'>,
+        dsl::ascii::alnum / dsl::lit_c<'_'>);
+    static constexpr auto value = lexy::as_string<string>;
 };
 
 struct ParameterList {
-  static constexpr auto rule =
-      dsl::parenthesized.list(dsl::p<ParameterItem>, dsl::sep(dsl::comma));
-  static constexpr auto value = lexy::as_list<vector<string>>;
+    static constexpr auto rule =
+        dsl::parenthesized.list(dsl::p<ParameterItem>, dsl::sep(dsl::comma));
+    static constexpr auto value = lexy::as_list<vector<string>>;
 };
 
 struct Sym {
-  static constexpr auto rule = [] {
-    auto sym_name =
-        dsl::p<SymName>; // 匹配带括号的形式参数列表，例如 "(abc,aaa,bcd,aab)"
-    auto bracket_opt = dsl::opt(
-        dsl::p<ParameterList>); // 如果不带参数，则会产生一个 dsl::nullopt
-    return sym_name + bracket_opt;
-  }();
-  // 需要将解析出来的内容，注入到Psym中去
-  static constexpr auto value = lexy::construct<Psym>;
+    static constexpr auto rule = [] {
+        auto sym_name =
+            dsl::p<SymName>;  // 匹配带括号的形式参数列表，例如 "(abc,aaa,bcd,aab)"
+        auto paramlist = dsl::opt(
+            dsl::p<ParameterList>);  // 如果不带参数，则会产生一个 dsl::nullopt
+        return sym_name + paramlist;
+    }();
+    // 需要将解析出来的内容，注入到Psym中去
+    static constexpr auto value = lexy::construct<Psym>;
 };
 
 struct NumberProduction {
-  static constexpr auto rule = dsl::integer<int> + dsl::opt(dsl::lit_c<'.'> >> dsl::integer<int>);
-  static constexpr auto value = lexy::construct<Number>;
+    static constexpr auto rule = dsl::integer<int> + dsl::opt(dsl::lit_c<'.'> >> dsl::integer<int>);
+    static constexpr auto value = lexy::construct<Number>;
 };
 
-struct MathExpr : lexy::expression_production {           // 目前这套规则没有实现带括号的表达式嵌套，但可以先尝试一下是否匹配
-  static constexpr auto whitespace = dsl::ascii::space;
-  static constexpr auto atom = dsl::p<Number>;     // 由于含有变量，atom应当指定为一个分支，除了Number以外，还可以是 ParameterItem
+// struct ParameterMappedItem {
+//   static constexpr auto rule = []{
+//     // auto literal_number = dsl::p<NumberProduction>;   // 数值字面量
+//     auto expression = dsl::p<MathExpr>;   // 数学表达式，输出expr指针
+//     auto defined_parameter_name = dsl::p<ParameterItem>;  // 左侧声明的形参
+//   }();
+// };
 
-  struct A_Expr : dsl::infix_op_left{
-    static constexpr auto op = dsl::op(dsl::lit_c<'+'>) / dsl::op(dsl::lit_c<'-'>);
-    using operand = dsl::atom;
-  };
+struct NestedExpression: lexy::transparent_production{
+  static constexpr auto whitespace = dsl::ascii::space | escaped_newline;
+  static constexpr auto rule = dsl::recurse<MathExpr>;
+  // static constexpr auto value = lexy::forward<>
+};
 
-  struct B_Expr : dsl::infix_op_left{
-    static constexpr auto op = dsl::op(dsl::lit_c<'*'>) / dsl::op(dsl::lit_c<'/'>);
-    using operand = A_Expr;
-  };
+struct MathExpr : lexy::expression_production {  // 目前这套规则没有实现带括号的表达式嵌套，但可以先尝试一下是否匹配
+    static constexpr auto whitespace = dsl::ascii::space;
+    static constexpr auto atom = []{
+      auto literal_number = dsl::p<Number>;           // 数值字面量
+      auto defined_param_name = dsl::p<ParameterItem>; // 左侧的形参名
+      auto nested_expr = dsl::parenthesized(dsl::p<NestedExpression>);    // 带括号的嵌套的表达式
+      return literal_number | defined_param_name | nested_expr | dsl::error<expected_operand>;
+    }();
+    
+    struct ExprItem : dsl::infix_op_left {
+        // 乘除相比加减有更高的优先级，将其整体称作 Item
+        static constexpr auto op = dsl::op(dsl::lit_c<'*'>) / dsl::op(dsl::lit_c<'/'>);
+        using operand = dsl::atom;
+    };
 
+    struct ExprRow : dsl::infix_op_left {
+        static constexpr auto op = dsl::op(dsl::lit_c<'+'>) / dsl::op(dsl::lit_c<'-'>);
+        using operand = ExprItem;
+    };
+};
+
+
+struct ParameterMappedList {
+    static constexpr auto rule = dsl::parenthesized.list(dsl::p<MathExpr>, dsl::sep(dsl::comma));
+    // static constexpr auto value = ;
 };
 
 struct SymMap {
-  // 调用时的表达式，对应实际参数Argument
-  // 与定义处不一样，实参可以是较为复杂的表达式，带优先级的四则运算以及初等函数
-  // 表达式的描述会非常复杂，包含数值常量(整型、浮点)，包含四则运算以及单目运算
-  // 这些都是好解决的，问题在于解析出的这些数据应该如何描述？
-  // 再SymDefineExpr中主要描述了映射的签名部分，这里需要描述映射的主体
+    static constexpr auto rule = [] {
+        auto sym_name = dsl::p<SymName>;
+        auto param_list = dsl::opt(dsl::p<ParameterMappedList>);
+        return sym_name + param_list;
+    }();
+    // static constexpr auto value = ;
 };
-} // namespace
+
+struct SymMap {
+    // 调用时的表达式，对应实际参数Argument
+    // 与定义处不一样，实参可以是较为复杂的表达式，带优先级的四则运算以及初等函数
+    // 表达式的描述会非常复杂，包含数值常量(整型、浮点)，包含四则运算以及单目运算
+    // 这些都是好解决的，问题在于解析出的这些数据应该如何描述？
+    // 再SymDefineExpr中主要描述了映射的签名部分，这里需要描述映射的主体
+};
+}  // namespace
 
 #endif
