aa0b341e5769d4f1661d67dbcf140af3a2b91323
windwhisper <1309896340@qq.com>
2024-09-03 23:02:35 +0800

    更改LSysCall并完成对控制字符的转换

--- src/demo.cpp
+++ src/demo.cpp
@@ -3,8 +3,6 @@
 using namespace std;
 
 int main(int argc, char** argv) {
-    // LSystem::D0L_System lsys("A(4,5)F(1,2,3)B(4)", "F(x,y,z)->A(x+z,y-x)F(x-2,z-3,y-1)B(y/2)\nA(x,y)->B(x/2)A(y/2,x/2)B(y+1)");
-
     vector<string> prods = {
         "F(x,y,z)->A(x+z,y-x)\\+F(x-2,z-3,y-1)/B(y/2)-",
         "A(x,y)->+-B(x/2)/A(y/2,x/2)B(y+1)"
@@ -13,6 +11,5 @@ int main(int argc, char** argv) {
 
     for (int i = 0; i < 3; i++) 
         cout << lsys.next() << endl;
-    
     return 0;
 }
