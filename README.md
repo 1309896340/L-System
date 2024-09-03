5364fbe6d9d50cb8881cfd42d4d3d0d2c297fd97
windwhisper <1309896340@qq.com>
2024-09-03 23:13:27 +0800

    修正readme.md错误

--- README.md
+++ README.md
@@ -33,7 +33,7 @@ Lindenmayer的继任者Prusinkiewicz在1992年著有 _The Algorithm Beauty of Pl
 #include "LSystem.hpp"
 using namespace std;
 int main(int argc, char** argv) {
-    LSystem::D0L_System lsys("A(4,5)F(1,2,3)B(4)", "F(x,y,z)->A(x+z,y-x)F(x-2,z-3,y-1)B(y/2)\\nA(x,y)->B(x/2)A(y/2,x/2)B(y+1)");
+    LSystem::D0L_System lsys("A(4,5)F(1,2,3)B(4)", "F(x,y,z)->A(x+z,y-x)F(x-2,z-3,y-1)B(y/2)\nA(x,y)->B(x/2)A(y/2,x/2)B(y+1)");
     for (int i = 0; i < 3; i++)
         cout << lsys.next() << endl;
     return 0;
--- include/LSystem.hpp
+++ include/LSystem.hpp
@@ -7,54 +7,53 @@
 
 #include "LSysParser.hpp"
 
-namespace LSystem{
+namespace LSystem {
 
 using namespace std;
 using LProduction = unordered_set<string>;
 
-void test_for_using() { cout << "ok" << endl; }
-
-
 class D0L_System {
-  LSysParser::config::LSystem lsystem;
-  string axiom;
-  string current_state; // 迭代的当前状态
+    LSysParser::config::LSystem lsystem;
+    string axiom;
+    string current_state;  // 迭代的当前状态
 
-public:
-  D0L_System(const string &axiom, const string &productions):axiom(axiom),current_state(axiom),lsystem() {
-    auto lsys_prods_string = lexy::string_input(productions);
-    auto lsys_res = lexy::parse<LSysParser::grammar::LSystem>(lsys_prods_string, lexy_ext::report_error);
-    assert(lsys_res.is_success());
-    this->lsystem = lsys_res.value();
-  }
-
-  D0L_System(const string &axiom, const vector<string> &productions):axiom(axiom),current_state(axiom),lsystem() {
-    vector<LSysParser::config::LProduction> prods;
-    for(const string &production : productions){
-      auto lsys_prod_string = lexy::string_input(production);
-      auto lsys_prod_res = lexy::parse<LSysParser::grammar::LProduction>(lsys_prod_string, lexy_ext::report_error);
-      assert(lsys_prod_res.is_success());
-      prods.push_back(lsys_prod_res.value());
+   public:
+    D0L_System(const string& axiom, const string& productions)
+        : axiom(axiom), current_state(axiom), lsystem() {
+        auto lsys_prods_string = lexy::string_input(productions);
+        auto lsys_res = lexy::parse<LSysParser::grammar::LSystem>(lsys_prods_string, lexy_ext::report_error);
+        assert(lsys_res.is_success());
+        this->lsystem = lsys_res.value();
     }
-    this->lsystem = LSysParser::config::LSystem(prods);
-  }
 
-  string next() {
-    this->current_state = this->iterate(this->current_state);
-    return this->current_state;
-  }
+    D0L_System(const string& axiom, const vector<string>& productions)
+        : axiom(axiom), current_state(axiom), lsystem() {
+        vector<LSysParser::config::LProduction> prods;
+        for (const string& production : productions) {
+            auto lsys_prod_string = lexy::string_input(production);
+            auto lsys_prod_res = lexy::parse<LSysParser::grammar::LProduction>(lsys_prod_string, lexy_ext::report_error);
+            assert(lsys_prod_res.is_success());
+            prods.push_back(lsys_prod_res.value());
+        }
+        this->lsystem = LSysParser::config::LSystem(prods);
+    }
 
-  void reset() { this->current_state = this->axiom; }
+    string next() {
+        this->current_state = this->iterate(this->current_state);
+        return this->current_state;
+    }
 
-  virtual string iterate(const string &p) {
-    auto input_string = lexy::string_input(p);
-    auto input_res = lexy::parse<LSysParser::grammar::LSysCall>(input_string, lexy_ext::report_error);
-    assert(input_res.is_success());
-    const LSysParser::config::LSysCall & syscall = input_res.value();
-    return syscall.apply(this->lsystem);
-  };
+    void reset() { this->current_state = this->axiom; }
+
+    virtual string iterate(const string& p) {
+        auto input_string = lexy::string_input(p);
+        auto input_res = lexy::parse<LSysParser::grammar::LSysCall>(input_string, lexy_ext::report_error);
+        assert(input_res.is_success());
+        const LSysParser::config::LSysCall& syscall = input_res.value();
+        return syscall.apply(this->lsystem);
+    };
 };
 
-} // namespace
+}  // namespace LSystem
 
 #endif
